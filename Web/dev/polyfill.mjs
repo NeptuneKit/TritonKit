5c3b940a32523419cee118b541470eb9bb529533
linhey <is.linhey@outlook.com>
2026-07-09 11:13:37 +0800

    chore: bump version to v0.2.10 and add Node 18 polyfill

---
+++ Web/dev/polyfill.mjs
@@ -0,0 +1,8 @@
+import crypto from "node:crypto";
+
+if (typeof crypto.hash !== "function") {
+  crypto.hash = function (algorithm, data, outputEncoding) {
+    const hash = crypto.createHash(algorithm).update(data);
+    return outputEncoding ? hash.digest(outputEncoding) : hash.digest();
+  };
+}
